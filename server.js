f78fcdf14b6e37475478cd02704c708e98e206e7
Ahaan Rawat <ahaanrawat@gmail.com>
2026-03-28 18:51:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
 const io = require('socket.io')(process.env.PORT || 3000, {
-    cors: { origin: "*", methods: ["GET", "POST"] }
+    cors: { origin: "*" }
 });
 
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log("Joined:", socket.id);
+    players[socket.id] = { x: 1000, y: 1000 };
     
-    players[socket.id] = { x: 1000, y: 1000, size: 15 };
-
     socket.on('move', (dir) => {
-        if (players[socket.id]) {
-            const s = 10;
-            if (dir === 'up')    players[socket.id].y -= s;
-            if (dir === 'down')  players[socket.id].y += s;
-            if (dir === 'left')  players[socket.id].x -= s;
-            if (dir === 'right') players[socket.id].x += s;
-        }
+        if (!players[socket.id]) return;
+        const speed = 10;
+        if (dir === 'up')    players[socket.id].y -= speed;
+        if (dir === 'down')  players[socket.id].y += speed;
+        if (dir === 'left')  players[socket.id].x -= speed;
+        if (dir === 'right') players[socket.id].x += speed;
     });
 
-    socket.on('disconnect', () => { delete players[socket.id]; });
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+    });
 });
 
 setInterval(() => {
