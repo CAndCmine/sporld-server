8648a4d107d4a78ac68aea00e4cd38db8ffc4303
Ahaan Rawat <ahaanrawat@gmail.com>
2026-03-29 08:43:23 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
-
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
-
-let players = {};
-
-// This serves your index.html automatically
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+const io = require('socket.io')(process.env.PORT || 3000, {
+    cors: { origin: "*" }
 });
+
+const players = {};
 
 io.on('connection', (socket) => {
     players[socket.id] = { x: 1000, y: 1000, angle: 0, name: "Guest" };
 
     socket.on('join', (name) => {
-        if (players[socket.id]) players[socket.id].name = name;
+        if (players[socket.id]) {
+            players[socket.id].name = name;
+        }
     });
 
     socket.on('move', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = Number(data.x);
-            players[socket.id].y = Number(data.y);
-            players[socket.id].angle = Number(data.angle);
+            players[socket.id].x = Number(data.x) || 0;
+            players[socket.id].y = Number(data.y) || 0;
+            players[socket.id].angle = Number(data.angle) || 0;
         }
     });
 
@@ -37,8 +29,3 @@ io.on('connection', (socket) => {
 setInterval(() => {
     io.emit('update', players);
 }, 15);
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
