536c0d3a2151a02ad1ff2e93f95c16bbea884075
Ahaan Rawat <ahaanrawat@gmail.com>
2026-03-29 18:16:18 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ const players = {};
 
 io.on('connection', (socket) => {
     players[socket.id] = { x: 1000, y: 1000, angle: 0, name: "Guest", score: 0 };
-    socket.on('join', name => { if (players[socket.id]) players[socket.id].name = name; });
-    socket.on('move', data => { if (players[socket.id]) Object.assign(players[socket.id], data); });
+    
+    socket.on('join', name => { 
+        if (players[socket.id]) players[socket.id].name = name; 
+    });
+    
+    socket.on('move', data => { 
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            players[socket.id].angle = data.angle;
+        } 
+    });
+    
     socket.on('disconnect', () => { delete players[socket.id]; });
 });
 
