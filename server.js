f63de8de778191e1c02c9b6f89d5230fdcabd074
Ahaan Rawat <ahaanrawat@gmail.com>
2026-03-28 21:19:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,14 @@
 const io = require('socket.io')(process.env.PORT || 3000, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
+    cors: { origin: "*" }
 });
 
 let players = {};
 
 io.on('connection', (socket) => {
-    console.log('Player connected:', socket.id);
-
-
-    players[socket.id] = { 
-        x: 1000, 
-        y: 1000, 
-        angle: 0, 
-        name: "Guest" 
-    };
+    players[socket.id] = { x: 1000, y: 1000, angle: 0, name: "Guest" };
 
     socket.on('join', (name) => {
-        if (players[socket.id]) {
-            players[socket.id].name = name.substring(0, 15) || "Guest";
-        }
+        if (players[socket.id]) players[socket.id].name = name;
     });
 
     socket.on('move', (data) => {
@@ -34,7 +21,6 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         delete players[socket.id];
-        console.log('Player disconnected:', socket.id);
     });
 });
 
