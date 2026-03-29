11f7e94080c301d30304c886f18a556d3a84583d
Ahaan Rawat <ahaanrawat@gmail.com>
2026-03-29 18:20:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server, { cors: { origin: "*" } });
 
 const players = {};
+const bullets = {};
+let bulletId = 0;
 
 io.on('connection', (socket) => {
     players[socket.id] = { x: 1000, y: 1000, angle: 0, name: "Guest", score: 0 };
@@ -19,13 +21,36 @@ io.on('connection', (socket) => {
             players[socket.id].angle = data.angle;
         } 
     });
+
+    socket.on('fire', data => {
+        const id = bulletId++;
+        bullets[id] = {
+            x: data.x,
+            y: data.y,
+            angle: data.angle,
+            speed: 15,
+            life: 100,
+            owner: socket.id
+        };
+    });
     
     socket.on('disconnect', () => { delete players[socket.id]; });
 });
 
 setInterval(() => {
     for (let id in players) players[id].score += 0.01;
-    io.emit('update', players);
+
+    for (let id in bullets) {
+        const b = bullets[id];
+        b.x += Math.cos(b.angle) * b.speed;
+        b.y += Math.sin(b.angle) * b.speed;
+        b.life--;
+        if (b.life <= 0 || b.x < 0 || b.x > 2000 || b.y < 0 || b.y > 2000) {
+            delete bullets[id];
+        }
+    }
+
+    io.emit('update', { players, bullets });
 }, 16);
 
 server.listen(process.env.PORT || 3000);
