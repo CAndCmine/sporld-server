94480a0d5644f791dd408e0cf9420248fc297a5b
Ahaan Rawat <ahaanrawat@gmail.com>
2026-03-29 12:02:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
-const io = require('socket.io')(process.env.PORT || 3000, {
-cors: { origin: "*" }
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: { origin: "*" }
 });
 
 const players = {};
 
 io.on('connection', (socket) => {
-players[socket.id] = { x: 1000, y: 1000, angle: 0, name: "Guest" };
+    console.log('User connected:', socket.id);
+    
+    // Initial data
+    players[socket.id] = { x: 1000, y: 1000, angle: 0, name: "Guest" };
 
-socket.on('join', (name) => {
-if (players[socket.id]) players[socket.id].name = name;
+    socket.on('join', (name) => {
+        if (players[socket.id]) players[socket.id].name = name;
+    });
+
+    socket.on('move', (data) => {
+        if (players[socket.id]) {
+            players[socket.id].x = data.x;
+            players[socket.id].y = data.y;
+            players[socket.id].angle = data.angle;
+        }
+    });
+
+    socket.on('disconnect', () => {
+        delete players[socket.id];
+        console.log('User disconnected:', socket.id);
+    });
 });
 
-socket.on('move', (data) => {
-if (players[socket.id]) {
-players[socket.id].x = data.x;
-players[socket.id].y = data.y;
-players[socket.id].angle = data.angle;
-}
-});
-
-socket.on('disconnect', () => {
-delete players[socket.id];
-});
-});
-
+// Update loop (60 times per second)
 setInterval(() => {
-io.emit('update', players);
+    io.emit('update', players);
 }, 16);
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
