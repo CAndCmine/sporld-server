8a36b94cc3a6eb484371fe608baa3a009ab0a1b7
Ahaan Rawat <ahaanrawat@gmail.com>
2026-03-29 18:14:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,26 +7,13 @@ const players = {};
 
 io.on('connection', (socket) => {
     players[socket.id] = { x: 1000, y: 1000, angle: 0, name: "Guest", score: 0 };
-
-    socket.on('join', name => { 
-        if (players[socket.id]) players[socket.id].name = name; 
-    });
-
-    socket.on('move', data => { 
-        if (players[socket.id]) { 
-            players[socket.id].x = data.x;
-            players[socket.id].y = data.y;
-            players[socket.id].angle = data.angle;
-        } 
-    });
-
+    socket.on('join', name => { if (players[socket.id]) players[socket.id].name = name; });
+    socket.on('move', data => { if (players[socket.id]) Object.assign(players[socket.id], data); });
     socket.on('disconnect', () => { delete players[socket.id]; });
 });
 
 setInterval(() => {
-    for (let id in players) {
-        players[id].score += 0.01;
-    }
+    for (let id in players) players[id].score += 0.01;
     io.emit('update', players);
 }, 16);
 
